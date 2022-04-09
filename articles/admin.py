a7a4f3cfdacffb043a10c279441e75cd1337e260
paka_coding <wnstjd5408@naver.com>
2022-04-10 01:49:20 +0900

    feat: 관리자페이지 수정

--- articles/admin.py
+++ articles/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import *
+
+
+@admin.register(Articles)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ["id", "category", "title", "created_at"]
+
+
+@admin.register(Comments)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ["id", "article"]
