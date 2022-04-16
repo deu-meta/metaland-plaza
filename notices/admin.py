b0a5f5aebe60637bf9009595fc380336f624a6d7
paka_coding <wnstjd5408@naver.com>
2022-04-17 02:30:45 +0900

    feat: viewcount 쿠기로해서 조회수중복방지, 인증권한설정

--- notices/admin.py
+++ notices/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Notices
+
+
+@admin.register(Notices)
+class NoticeAdmin(admin.ModelAdmin):
+    list_display = ["id", "title", "notion"]
+    list_per_page = 10
--- notices/urls.py
+++ notices/urls.py
@@ -1,3 +1,11 @@
-from django.urls import URLPattern, include, path
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
 
-urlpatterns = []
+from .views import NoticeViewSet
+
+router = DefaultRouter()
+
+
+router.register("", NoticeViewSet, basename="notices")
+
+urlpatterns = [path("", include(router.urls))]
