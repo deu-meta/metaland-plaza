17e47233a2369fd01548e2285f9c174a44cdefe2
paka_coding <wnstjd5408@naver.com>
2022-05-18 18:10:10 +0900

    feat: article 검색기능 및 날짜순으로 ordering

--- articles/views.py
+++ articles/views.py
@@ -1,5 +1,7 @@
 # Create your views here.
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import permissions, viewsets
+from rest_framework.filters import OrderingFilter, SearchFilter
 
 from .permissions import IsUserOrReadOnly
 from .serializers import *
@@ -9,6 +11,10 @@ class ArticleViewSet(viewsets.ModelViewSet):
     queryset = Article.objects.all()
     serializer_class = ArticleSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsUserOrReadOnly]
+    filter_backends = [OrderingFilter, SearchFilter]
+
+    ordering_fields = ["created_at"]
+    search_fields = ["title"]
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
