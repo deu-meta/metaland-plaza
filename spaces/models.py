5a70e894daf0f9371671a121c167d0b894c460d1
solo5star <solo_5@naver.com>
2022-06-02 04:05:33 +0900

    fix: `Space`: rename all images to random uuid4

--- spaces/models.py
+++ spaces/models.py
@@ -1,4 +1,12 @@
+import os
+import uuid
+
+from django.conf import settings
 from django.db import models
+
+
+def get_image_path(instance, filename) -> str:
+    return os.path.join("spaces", "{}.{}".format(uuid.uuid4(), filename.split(".")[-1]))
 
 
 class Space(models.Model):
@@ -7,7 +15,7 @@ class Space(models.Model):
 
     name = models.CharField(max_length=100, primary_key=True)
     type = models.CharField(max_length=30, choices=CATEGORY_CHOICES, null=False)
-    thumbnail = models.ImageField(default="default_image.jpg")
+    thumbnail = models.ImageField(default="default_image.jpg", upload_to=get_image_path)
     short_introduce = models.CharField(max_length=200, default="소개없음")
     long_introduce = models.TextField(default="글 없음")
 
